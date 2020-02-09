428200a61b4c330065813228afdcfd2d73bf295a
Lionel Adams <adamslionel@hotmail.co.uk>
2020-02-09 17:15:33 +0000

    chore: update rest-api and mock data

--- server/src/index.express.api.ts
+++ server/src/index.express.api.ts
@@ -1,7 +1,7 @@
-import express from "express";
+import express from 'express';
 // import bodyParser from "body-parser";
 
-import { listings } from "./listings";
+import { listings } from './listings';
 
 const app = express();
 const PORT = 9000; // TODO: set by .env config
@@ -12,26 +12,26 @@ let filteredListings = [...listings];
 // app.use(bodyParser.json());
 app.use(express.json());
 
-app.get("/", (_req, res) => {
-  return res.send("House Listings");
+app.get('/', (_req, res) => {
+  return res.send('House Listings');
 });
 
-app.get("/listings", (_req, res) => {
+app.get('/listings', (_req, res) => {
   return res.send(filteredListings);
 });
 
 // DELETE
-app.post("/delete-listing", (req, res) => {
+app.post('/delete-listing', (req, res) => {
   const id: string = req.body.id;
 
-  const deletedListing = filteredListings.find(listing => listing.id === id);
+  const deletedListing = filteredListings.find(listing => listing._id === id);
 
   if (!deletedListing) {
-    return res.send("Couldnt find listing");
+    return res.send('Couldnt find listing');
   }
 
   // update in-memory listings
-  filteredListings = filteredListings.filter(listing => listing.id !== id);
+  filteredListings = filteredListings.filter(listing => listing._id !== id);
 
   // return the deleted listing
   return res.send(deletedListing);
