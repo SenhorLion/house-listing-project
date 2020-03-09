9cec12b4f140c38d22629f08caa401477db9e9d9
Lionel Adams <lionel.adams@gmail.com>
2020-03-09 18:53:38 +0000

    refactor: scaffold out new resolvers and types for viewer

--- server/src/graphql/resolvers/index.ts
+++ server/src/graphql/resolvers/index.ts
@@ -1,6 +1,4 @@
 import merge from 'lodash.merge';
+import { viewerResolvers } from './Viewer';
 
-import { listingResolvers } from './Listing';
-import { userResolvers } from './User';
-
-export const resolvers = merge(listingResolvers, userResolvers);
+export const resolvers = merge(viewerResolvers);
--- server/src/graphql/typeDefs.ts
+++ server/src/graphql/typeDefs.ts
@@ -1,37 +1,12 @@
 import { gql } from 'apollo-server-express';
 
 export const typeDefs = gql`
-  type Listing {
-    id: ID!
-    title: String!
-    description: String!
-    image: String!
-    host: String!
-    address: String!
-    country: String!
-    admin: String!
-    city: String!
-    bookings: [ID]
-    price: Int!
-  }
-  type User {
-    id: ID!
-    token: String!
-    name: String!
-    avatar: String!
-    contact: String!
-    walletId: String
-    income: Int
-    bookings: [ID]
-    listings: [ID]
-  }
-
   type Query {
-    listings: [Listing!]!
-    users: [User!]!
+    authUrl: String!
   }
 
   type Mutation {
-    deleteListing(id: ID!): Listing!
+    logIn: String!
+    logOut: String!
   }
 `;
